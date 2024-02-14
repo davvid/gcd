1a932ccae75764205f7ad8f86f623829b3fd7f9c
David Aguilar <davvid@gmail.com>
2024-02-14 08:29:19 -0800

    gcd: git worktree navigator

---
+++ README.md
@@ -0,0 +1,57 @@
+# gcd - Git worktree navigator
+
+`gcd` is lets you quickly navigate to Git worktrees on your filesystem.
+
+## Dependencies
+
+* [eza](https://github.com/eza-community/eza) (required)
+
+* [fzf](https://github.com/junegunn/fzf#search-syntax) (required)
+
+* [fd](https://github.com/sharkdp/fd) (optional)
+
+```bash
+sudo apt install eza fzf fd-find
+```
+
+## Installation
+
+* Clone this repository to `~/.config/git/gcd`:
+
+```bash
+mkdir -p ~/.config/git
+git clone https://gitlab.com/davvid/gcd.git ~/.config/git/gcd
+```
+
+* Edit your `~/.bashrc` or `~/.zshrc` and add this snippet:
+
+```bash
+if test -f ~/.config/git/gcd/gcd.sh
+then
+    source ~/.config/git/gcd/gcd.sh
+fi
+```
+
+## Configuration
+
+`gcd` searches for worktrees in `$HOME` by default.
+
+`gcd` can be configured to search in other directories by setting the
+`gcd.paths` git config variable:
+
+```bash
+git config --global --add gcd.paths '$HOME/src'
+git config --global --add gcd.paths '$HOME/dev'
+```
+
+`$VARIABLES` are expanded by `gcd` so that you can reuse your git configuration
+in more scenarios.
+
+`gcd` traverses two levels deep into the configured paths by default.
+You can configure `gcd` to search deeper by setting the `gcd.depth` variable.
+
+```bash
+git config --global gcd.depth 3
+```
+
+*NOTE*: higher depth values cause more disk IO and can have an impact on performance.
---
+++ gcd.sh
@@ -0,0 +1,73 @@
+# This script is intended to be sourced into your ~/.zshrc or ~/.bashrc.
+
+# Search for worktrees and change directories.
+gcd () {
+    __gcd_worktree=$(__gcd_worktrees |
+        uniq |
+        fzf \
+            --ansi \
+            --border=none \
+            --cycle \
+            --filepath-word \
+            --info=inline-right \
+            --keep-right \
+            --preview='eza --all --color=always --git-ignore --group-directories-first --icons {}' \
+            --preview-window=down,33%,border-none \
+            --query="$*" \
+            --scheme=path \
+            --tiebreak=end,chunk,length
+        )
+    if test -n "${__gcd_worktree}"
+    then
+        cd "${__gcd_worktree}"
+    fi
+}
+
+# Find worktrees and print their paths to stdout.
+__gcd_worktrees () {
+    restore_zsh_wordsplit=
+    if test -n "${ZSH_VERSION}" && test -z "$(setopt | grep shwordsplit)"
+    then
+        restore_zsh_wordsplit=true
+        set -o shwordsplit
+    fi
+
+    gcd_paths=$(git config --get-all gcd.paths | envsubst)
+    if test -z "${gcd_paths}"
+    then
+        gcd_paths="${HOME}"
+    fi
+    OIFS="${IFS}"
+    IFS='
+'
+    set --
+    for gcd_path in ${gcd_paths}
+    do
+        set -- "$@" "${gcd_path}"
+    done
+    IFS="${OIFS}"
+
+    gcd_depth=$(git config gcd.depth || echo 2)
+    # Add + 1 to account for .git.
+    gcd_depth=$((gcd_depth + 1))
+    fdfind_cmd=
+    fdfind_args="--color=never --case-sensitive --hidden --no-ignore --max-depth=${gcd_depth}"
+    if type fd >/dev/null 2>&1
+    then
+        fdfind_cmd=fd
+    elif type fdfind >/dev/null 2>&1
+    then
+        fdfind_cmd=fdfind
+    fi
+    if test -n  "${fdfind_cmd}"
+    then
+        "${fdfind_cmd}" ${fdfind_args} '^\.git$' "$@" | xargs -P 4 -n 1 dirname
+    else
+        find -L "$@" -maxdepth ${gcd_depth} -name .git -printf '%h\n' 2>/dev/null
+    fi
+
+    if test -n "${restore_zsh_wordsplit}"
+    then
+        set +o shwordsplit
+    fi
+}
